e71c491525e7d41655d2e6bf79118d5eb8fedfc4
Martin Kamleithner <martin+kamleithner@diagnosia.com>
2020-10-10 18:06:50 +0200

    more tests

--- test/diffutil_test.dart
+++ test/diffutil_test.dart
@@ -1,29 +1,139 @@
-import 'package:diffutil_dart/diffutil.dart';
+import 'package:diffutil_sliverlist/diffutil_sliverlist.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  final result = calculateListDiff([1, 2, 3], [3, 1, 0, 5]);
+  testWidgets("does not allow non-unique keys when using fromKeyedWidgetList",
+      (tester) async {
+    await tester.pumpWidget(Builder(
+      builder: (context) => DiffUtilSliverList.fromKeyedWidgetList(
+          children: [
+            Container(
+              key: Key('a'),
+            ),
+            Container(
+              key: Key('a'),
+            ),
+          ],
+          insertAnimationBuilder: (context, animation, widget) => widget,
+          removeAnimationBuilder: (context, animation, widget) => widget),
+    ));
 
-  result.dispatchUpdatesTo(MyConsumer());
-}
+    final dynamic error = tester.takeException();
 
-class MyConsumer implements ListUpdateCallback {
-  @override
-  void onChanged(int position, int count, Object payload) {
-    print("$position changed $count times: $payload");
-  }
+    expect(error, isAssertionError);
+  });
 
-  @override
-  void onInserted(int position, int count) {
-    print("on position $position inserted $count times");
-  }
+  testWidgets("does not throw error when keys are unique", (tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: CustomScrollView(slivers: [
+        DiffUtilSliverList.fromKeyedWidgetList(
+            children: [
+              Container(
+                key: Key('a'),
+              ),
+              Container(
+                key: Key('b'),
+              ),
+            ],
+            insertAnimationBuilder: (context, animation, widget) => widget,
+            removeAnimationBuilder: (context, animation, widget) => widget),
+      ]),
+    ));
 
-  @override
-  void onMoved(int fromPosition, int toPosition) {
-    print("item moved from $fromPosition to $toPosition");
-  }
+    final dynamic error = tester.takeException();
 
-  @override
-  void onRemoved(int position, int count) {
-    print("$count items removed at $position");
-  }
+    expect(error, isNull);
+  });
+
+  testWidgets("insert builder called", (tester) async {
+    int insertCount = 0;
+
+    await tester.pumpWidget(MaterialApp(
+      home: CustomScrollView(slivers: [
+        DiffUtilSliverList.fromKeyedWidgetList(
+            children: [
+              Container(
+                key: Key('a'),
+              ),
+            ],
+            insertAnimationBuilder: (context, animation, widget) => widget,
+            removeAnimationBuilder: (context, animation, widget) => widget),
+      ]),
+    ));
+
+    await tester.pumpWidget(MaterialApp(
+      home: CustomScrollView(slivers: [
+        DiffUtilSliverList.fromKeyedWidgetList(
+            children: [
+              Container(
+                key: Key('a'),
+              ),
+              Container(
+                key: Key('b'),
+              ),
+            ],
+            insertAnimationBuilder: (context, animation, widget) {
+              if (animation.isCompleted) {
+                expect(widget.key, Key('a'));
+              } else {
+                expect(widget.key, Key('b'));
+              }
+              insertCount++;
+              return widget;
+            },
+            removeAnimationBuilder: (context, animation, widget) {
+              fail("removeAnimationBuilder should not be called");
+            }),
+      ]),
+    ));
+
+    await tester.pumpAndSettle();
+
+    expect(insertCount, 2);
+  });
+
+  testWidgets("remove builder called", (tester) async {
+    int removeCount = 0;
+
+    await tester.pumpWidget(MaterialApp(
+      home: CustomScrollView(slivers: [
+        DiffUtilSliverList.fromKeyedWidgetList(
+            children: [
+              Container(
+                key: Key('a'),
+              ),
+              Container(
+                key: Key('b'),
+              ),
+            ],
+            insertAnimationBuilder: (context, animation, widget) => widget,
+            removeAnimationBuilder: (context, animation, widget) => widget),
+      ]),
+    ));
+
+    await tester.pumpWidget(MaterialApp(
+      home: CustomScrollView(slivers: [
+        DiffUtilSliverList.fromKeyedWidgetList(
+            children: [
+              Container(
+                key: Key('b'),
+              ),
+            ],
+            insertAnimationBuilder: (context, animation, widget) {
+              expect(animation.isCompleted, isTrue);
+              return widget;
+            },
+            removeAnimationBuilder: (context, animation, widget) {
+              expect(widget.key, Key('a'));
+              expect(animation.isCompleted, isFalse);
+              removeCount++;
+              return widget;
+            }),
+      ]),
+    ));
+
+    expect(removeCount, 1);
+  });
 }
